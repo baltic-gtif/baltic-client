9d98e40111174661094066b68f88420769af042f
lubojr <lubojr@seznam.cz>
2026-04-14 18:58:36 +0000

    Deploying to gh-pages from @ baltic-gtif/baltic-client@75604cd3a2e23805289bfad62939aa39966ea905 🚀

--- assets/chunks/ssrBoot-Zgc_Ttvi.twAXFfoy.js
+++
@@ -1,1 +0,0 @@
-import{v as t,E as e,S as s,ap as n}from"./framework.BpYuAWAW.js";function i(){const o=e(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:n(()=>o.value?void 0:{transition:"none !important"}),isBooted:s(o)}}export{i as u};
